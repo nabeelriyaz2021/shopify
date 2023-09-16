65df22752356e80153c484b490bdc4dcf380a52e
nabeelriyaz2021 <nabeelriyaz369@gmail.com>
2023-09-16 19:03:36 +0530

    y

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js"
+import cors from 'cors';
+dotenv.config();
+const app = express();
+app.use(cors())
+app.use(express.json());
+app.use(morgan("dev"));
+connectDB();
+//routes
+app.use("/api/v1/category",categoryRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/product", productRoutes);
+app.get('/',(req,res)=>{
+    res.send("Welcome to Shopify")
+
+});
+const PORT=process.env.PORT || 8080
+app.listen(PORT,()=>{
+    console.log(`Server running at port ${PORT}`);
+})
